aae9bdf8f2552f04dbcb7a30191aef7dac3009d9
1h2w8 <toscatland@gmail.com>
2014-05-13 22:48:30 +0000

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -93,6 +93,18 @@ describe "Authentication" do
         end
       end
 
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+
       describe "for signed in users" do
         let(:user) { FactoryGirl.create(:user) }
         let(:new_user) { FactoryGirl.attributes_for(:user) }
@@ -123,6 +135,16 @@ describe "Authentication" do
 
         describe "visiting the user index" do
           before { visit users_path }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
           it { should have_title('Sign in') }
         end
       end
